4c5d6a826da5e04b8358b8657d15f2c04af3a16d
Nikita Kant <nikitakantyukov@gmail.com>
2020-08-27 22:05:03 +0300

    Добавьте файлы проекта.

---
+++ ExchangeProvider/ExchangeProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeProvider
+{
+    public class ExchangeProvider
+    {
+        /// <summary>
+        /// Курс у.е.
+        /// </summary>
+        public double Cource => 64.25;
+
+        /// <summary>
+        /// Текущая комиссия
+        /// </summary>
+        public double GetCommission(double value)
+        {
+            if (value < 1000)
+            {
+                return 4.0;
+            }
+
+            if (value < 10000)
+            {
+                return 3.0;
+            }
+
+            if (value < 50000)
+            {
+                return 2.0;
+            }
+
+            if (value < 100000)
+            {
+                return 1.0;
+            }
+
+            return 0.3;
+        }
+
+        /// <summary>
+        /// Перевести деньги
+        /// </summary>
+        public double Exchange(double value)
+        {
+            return (value / Cource) * (1 - GetCommission(value) / 100);
+        }
+    }
+}
---
+++ ExchangeProvider/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExchangeProvider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сумму в рублях: ");
+            var rubStrValue = Console.ReadLine();
+            var rubValue = 0.0;
+            if (!double.TryParse(rubStrValue, out rubValue))
+            {
+                throw new InvalidCastException($"Указанная сумма '{rubStrValue}' не является валидным числом");
+            }
+
+            var provider = new ExchangeProvider();
+            var result = provider.Exchange(rubValue);
+
+            Console.WriteLine($"Сумма в руб: {rubValue:# ##0.00}| курс: {provider.Cource:# ##0.00}| комиссия: {provider.GetCommission(rubValue):# ##0.00}| cумма в у.е.: {result:# ##0.00}");
+        }
+    }
+}
